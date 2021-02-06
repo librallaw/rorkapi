e9cca34ea632d224df98d8e05e37c290aa7dd70f
librallaw <liballaw@yahoo.com>
2021-02-06 05:03:59 +0100

    added key for user purchased product to product details payload

--- routes/api.php
+++ routes/api.php
@@ -44,7 +44,7 @@ Route::get('/rork/product/all',  'RORK\ProductController@viewProducts');
 
 Route::get('/rork/product/featured/all',  'RORK\ProductController@viewFeaturedProducts');
 
-Route::get('/rork/product/single/{product_id},{user_id}',  'RORK\ProductController@productDetails');
+Route::get('/rork/product/single/{product_id}/{user_id}',  'RORK\ProductController@productDetails');
 
 Route::get('/rork/product/related/{product_id}',  'RORK\ProductController@productRelation');
 Route::get('/rork/product/categories/{product_id}',  'RORK\ProductController@SingleCategoryProduct');
